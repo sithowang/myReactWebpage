75e81f795a44f1892e76ca56d6c5a1274cccd2aa
SIT Ho Wang <165016358+zouwei328@users.noreply.github.com>
2025-08-28 09:54:38 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://zouwei328.github.io/myReactWebpage.git",
+  base:"https://zouwei328.github.io/myReactWebpage",
 })
